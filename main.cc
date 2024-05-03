daeef5a912bb06e622866ca23ba431079da30eb2
rampurgeCSUMB <98712477+rampurgeCSUMB@users.noreply.github.com>
2024-05-02 22:54:05 -0700

    Update main.cc

--- main.cc
+++ main.cc
@@ -1,6 +1,6 @@
 /*
  * INSTRUCTION:
- *     This is a C++ starting code for hw 10_1.
+ *     This is a C++ starting code for hw 11_1.
  */
 
 // Finish the head comment with Abstract, Name, and Date.
